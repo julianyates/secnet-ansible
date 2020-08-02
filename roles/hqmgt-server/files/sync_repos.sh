21dd0f43b416769954ee9b1624315c674f1292ac
Julian yates <j.yates@secnetwork.org>
2020-08-01 23:52:01 -0400

    finish creating hqmgt role and tested

---
+++ roles/hqmgt-server/files/sync_repos.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Downloading RPM GPG Key"
+wget -P /opt/repos/centos/8/ https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
+
+echo "Syncing DNF Repository"
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/AppStream/x86_64/ /opt/repos/centos/8/AppStream/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/BaseOS/x86_64/ /opt/repos/centos/8/BaseOS/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/centosplus/x86_64/ /opt/repos/centos/8/centosplus/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/cloud/x86_64/ /opt/repos/centos/8/cloud/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/configmanagement/x86_64/ /opt/repos/centos/8/configmanagement/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/Devel/x86_64/ /opt/repos/centos/8/Devel/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/extras/x86_64/ /opt/repos/centos/8/extras/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/HighAvailability/x86_64/ /opt/repos/centos/8/HighAvailability/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/messaging/x86_64/ /opt/repos/centos/8/messaging/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/opstools/x86_64/ /opt/repos/centos/8/opstools/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/PowerTools/x86_64/ /opt/repos/centos/8/PowerTools/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/storage/x86_64/ /opt/repos/centos/8/storage/x86_64/
+rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/virt/x86_64/ /opt/repos/centos/8/virt/x86_64/
+
+echo "Syncing EPEL Repository"
+rsync  -avSHP --delete rsync://mirror.steadfastnet.com/epel/RPM-GPG-KEY-EPEL-8 /opt/repos/epel/RPM-GPG-KEY-EPEL-8
+rsync  -avSHP --delete rsync://mirror.steadfastnet.com/epel/8/Everything/x86_64/ /opt/repos/epel/8/Everything/x86_64
+
+exit 0

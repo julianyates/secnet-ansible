df01ea45a8113db27509cc17164751007368d3b7
Julian Yates <j.yates@secnetwork.org>
2020-09-04 01:02:29 -0400

    updating sync_repos script to add nginx repo

--- roles/hqmgt-server/files/sync_repos.sh
+++ roles/hqmgt-server/files/sync_repos.sh
@@ -4,12 +4,13 @@ CREATE="/opt/repos/centos/8 /opt/repos/centos/8/AppStream/x86_64 /opt/repos/cent
 
 for x in $CREATE; do
 	if [ ! -d $x ]; then
+		echo "Creating $x Directory"
 		mkdir -p $x
 	fi
 done
 
 echo "Downloading RPM GPG Key"
-wget -P /opt/repos/centos/8/ https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
+wget -P /opt/repos/centos/8/RPM-GPG-KEY-CentOS-Official https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
 
 echo "Syncing DNF Repository"
 rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/AppStream/x86_64/ /opt/repos/centos/8/AppStream/x86_64/
@@ -29,5 +30,9 @@ rsync  -avSHP --delete rsync://mirror.vcu.edu/centos/8/virt/x86_64/ /opt/repos/c
 echo "Syncing EPEL Repository"
 rsync  -avSHP --delete rsync://mirror.steadfastnet.com/epel/RPM-GPG-KEY-EPEL-8 /opt/repos/epel/RPM-GPG-KEY-EPEL-8
 rsync  -avSHP --delete rsync://mirror.steadfastnet.com/epel/8/Everything/x86_64/ /opt/repos/epel/8/Everything/x86_64
+
+echo "Syncing NGINX Repository"
+wget --no-parent -m http://nginx.org/packages/mainline/centos/8/x86_64/ -P /opt/repos
+curl https://nginx.org/keys/nginx_signing.key > /opt/repos/nginx.org/nginx_signing.key
 
 exit 0
